fa0c0c0ba5931223d6ea23b53438bbbd34cc5ef5
Célien <levequecelien3@gmail>
2022-12-01 14:25:56 +0100

    Ajoute front -> C# et modif href en asp-controler

---
+++ TheTemptation/TheTemptation/Controllers/ResultatController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TheTemptation.Controllers
+{
+    public class ResultatController : Controller
+    {
+        // GET: Resultat
+        public ActionResult Resultat()
+        {
+            return View();
+        }
+
+        // GET: Resultat/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Resultat/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Resultat/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Resultat/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Resultat/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Resultat/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Resultat/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
